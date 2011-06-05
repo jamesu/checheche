d10ee0d8ed2c5429928deabd5d18c1dcabb0c4b2
James Urquhart <jamesu@gmail.com>
2011-06-05 15:15:22 +0100

    Latest progress

--- main.rb
+++ main.rb
@@ -2,7 +2,29 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 require 'sinatra'
+include Magick
 
 get '/' do
-  'hello world'
+  haml :index
+end
+
+get '/style.css' do
+  send_file 'style.css'
+end
+
+post '/cheify' do
+  if params[:data].nil? or params[:data][:tempfile].nil?
+    redirect '/'
+  end
+
+  image_data = params[:data][:tempfile].read
+  images = Image.from_blob(image_data)
+  if images.nil? or images.empty?
+    redirect '/'
+  else
+    image = images[0]
+    image.format = 'PNG'
+    content_type 'image/png'
+    image.threshold(MaxRGB * 0.5).to_blob
+  end
 end
